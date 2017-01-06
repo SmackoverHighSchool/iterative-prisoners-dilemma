7922a62fe8faa841b19f1aeede3e47f5d79abd62
kaitlinjones1999 <kaitlin.jones@smackover.net>
2017-01-06 17:02:09 -0600

    Update team14.py

--- team14.py
+++ team14.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'The strategy is too collude'
+team_name = 'Team 13' # Only 10 chars displayed.
+strategy_name = 'Collude if oppent has higher score'
+strategy_description = 'Collude when my score is lower than their score'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
